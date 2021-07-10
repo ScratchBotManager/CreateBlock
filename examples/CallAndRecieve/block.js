350ad87aa66cb4fe4fa15b89a6a4a68224153f5f
ScratchBotManager <84136976+ScratchBotManager@users.noreply.github.com>
2021-07-10 09:42:24 -0400

    Create block.js

---
+++ examples/CallAndRecieve/block.js
@@ -0,0 +1,39 @@
+Blockly.Blocks['call'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Call")
+        .appendField(new Blockly.FieldTextInput("call1"), "callstring");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['recieve'] = {
+  init: function() {
+    this.appendStatementInput("RecieveInput")
+        .setCheck(null)
+        .appendField("Recieve")
+        .appendField(new Blockly.FieldTextInput("call1"), "Reciever");
+    this.setColour(225);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.JavaScript['call'] = function(block) {
+  var text_callstring = block.getFieldValue('callstring');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['recieve'] = function(block) {
+  var text_reciever = block.getFieldValue('Reciever');
+  var statements_recieveinput = Blockly.JavaScript.statementToCode(block, 'RecieveInput');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
