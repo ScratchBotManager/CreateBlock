05d4d38d3bd0604c3be4e3f38a1453e311ba95ac
HackitHatchet <84136976+ScratchBotManager@users.noreply.github.com>
2021-08-10 16:48:51 -0400

    Update custom.js

--- code/custom.js
+++ code/custom.js
@@ -23,10 +23,11 @@ Blockly.Blocks['blockvalue'] = {
   }
 };
 
+//define
 Blockly.JavaScript['valuetoblock'] = function(block) {
   var value_blocks = Blockly.JavaScript.valueToCode(block, 'blocks', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = value_blocks;
+  var code = '...';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
@@ -34,7 +35,7 @@ Blockly.JavaScript['valuetoblock'] = function(block) {
 Blockly.JavaScript['blockvalue'] = function(block) {
   var statements_value = Blockly.JavaScript.statementToCode(block, 'value');
   // TODO: Assemble JavaScript into code variable.
-  var code = statements_value;
+  var code = '...';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
